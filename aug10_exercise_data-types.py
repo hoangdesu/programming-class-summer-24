1128b632ab30337bc5dd1a182bffbf7d9b5ed3b9
hoangdesu <hoangdesu@gmail.com>
2024-08-11 01:58:01 +0700

    string format, data types, type casting

---
+++ aug10_exercise_data-types.py
@@ -0,0 +1,76 @@
+# 1. enter your first name: hoang
+# 2. enter your family name: nguyen
+# 3. enter your middle name: quoc
+
+# => Have a nice day, Nguyen Quoc Hoang! 🤩
+
+# variable: container for a value
+
+
+# someone_name = 'brian'
+
+# first_name = input('What is your first name: ')
+# last_name = input('What is your last name: ')
+# middle_name = input('What is your middle name: ')
+
+# string format f"{value}"
+# string methods: capitalize first letter
+# full_name = f'{last_name.capitalize()} {middle_name.capitalize()} {first_name.capitalize()}'
+
+# print(f'Have a nice day, {full_name}! 🤩')
+
+# string concatenation: combining strings
+# print('hi ' + someone_name + ' ' + last_name)
+
+
+# Problems with numbers
+
+# a = int(input('Enter number a: '))
+# b = int(input('Enter number b: '))
+# # c = a + b
+
+# print(a + b)
+
+
+# Data types:
+#     - string: characters
+#     - numbers:
+#         - integer (int): whole numbers => 10, 11, 0, -99
+#         - float: decimal numbers => 3.5, -9.1
+#     - boolean: True / False => 1 / 0
+
+
+# Type casting: convert between data types
+# int(str): convert str -> int
+# str(int): int -> str
+
+# HCM -> VT: 120km
+# 45km/h => 2.67 hours
+# 60km/h => 2 hrs
+
+# Enter distance (km): 120
+# Enter speed (km/h): 45
+# => Time taken to drive 120 km is 2.67 hours.
+
+# print(120 / 45) # division
+
+# distance = int(input('Enter distance (km): '))
+# speed = int(input('Enter speed (km/h): '))
+# time = distance / speed
+
+# print(f'Time taken to drive {distance} km is {time} hours.')
+
+# Pythagoras
+# a^2 + b^2 = c^2
+# c = math.sqrt(c^2)
+
+
+# a = 7
+# print(a ** 2) square
+
+# import math
+
+# square root: can bac 2
+# b = math.sqrt(64)
+
+# print(b)
---
+++ hello.py
@@ -0,0 +1,36 @@
+# output value
+print('hehe')
+print('how is your day? ')
+print('🫶 🧡')
+
+
+# declare and define variables
+# variable: biến
+name = 'brian'
+hobby = 'piano'
+print('hi my name is ' + name + '. I like to play the ' + hobby)
+
+
+# comment: for human to take notes -> ignored by python
+# shortcut: ctrl /
+
+# leave some empty lines in between to separate out the parts
+
+
+# getting user input
+
+my_name = input('what is your name: ') # prompt
+print("have a nice day " + my_name + ' !!!')
+
+# have a nice day Kevin / Beo / Hilary !!!
+
+# Bugs: software error
+
+# 'string': series of characters
+# "string"
+
+# 'single quote'
+# "double quote"
+
+
+
