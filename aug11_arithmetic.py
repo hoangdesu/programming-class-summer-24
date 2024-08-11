fbf526b776ce96029d798e6733030959bc08e829
hoangdesu <hoangdesu@gmail.com>
2024-08-11 11:07:11 +0700

    arithmetic, variable naming, exercises, hw

---
+++ aug11_arithmetic.py
@@ -0,0 +1,138 @@
+# + - * /
+# a = 10
+# b = 5.5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b) # always float => float division
+# print(a // b) # int division
+# print(a ** b) # base: a, power: b
+
+# Remainder: tính phần dư
+# print(10 % 4) # modulus / mod
+
+# precision: độ chính xác
+
+# int + int = int
+# int + float = float
+
+# num = int(input('Enter a number: '))
+
+# is this number divisible by 2
+# if num % 2 == 0:
+#     print('this is an even number')
+# else:
+#     print('this is an odd number')
+
+# 1 dog + 2 dogs 
+# 1 dog + 1 cat
+
+# print('hello' + str(2))
+
+# print(float('4') + int('3'))
+
+# Hi what is your name: -> brian      => name
+# When were you born, Brian? 1995     => birth_year
+# Oh you are 29 years old now!
+
+# String methods
+progLanguage = 'python is a great Programming LANGUAGE'
+# print(progLanguage.capitalize())
+# print(progLanguage.title())
+# print(progLanguage.upper())
+# print(progLanguage.lower())
+
+# print(progLanguage) autocomplete: enter | tab
+
+# Variable naming convention: quy tắc đặt tên cho biến
+
+name = 'Beo'
+Name = 'Phu'
+namE = 'Hieu'
+print('name', name)
+print('Name', Name)
+print('namE', namE)
+
+
+
+my_name = 'Hoang'
+
+this_is_a_very_long_variable_name = ''
+thisIsAVeryLongVariableName = ''
+
+# thisisaverylongvariablename
+
+# snake_case : _ underscore
+
+# camelCase: 
+
+# case-sensitive
+
+# prog_lang_1 = ''
+# prog_lang_2 = ''
+# prog_lang_3 = ''
+# prog_lang_4 = ''
+# prog_lang_5 = ''
+
+# print(prog_lang_4)
+
+# ten bien ko dc phep bat dau bang so hoac ky tu dac biet
+# ten bien ko dc co ki tu dac biet
+
+student1 = '' # ok
+
+# not ok
+# 1student = ''
+# *student = ''
+# student$ = ''
+# student@ = ''
+
+# a = ?
+# b = ?
+
+
+# Simple calculator
+# enter number a: 5
+# enter number b: 100
+
+# 5 + 100 = 105
+# 5 - 100 = -95
+# 5 x 100 = 500
+# 5 : 100 = 0.05
+
+# # 2 input
+# total = a + b
+# difference = a - b
+# multiplication = a * b
+# division = a / b
+
+# f"{a} + {b} = {c}"
+# f"{a} - {b} = {c}"
+# f"{a} x {b} = {c}"
+# f"{a} : {b} = {c}"
+
+# Homework: currency converter
+# Convert USD to VND. Please enter amount in USD: 1000
+# 1000 USD = 25000000 VND
+
+# Homework 2: convert hours to hours and minutes
+# 2.5 hours = 2 hours 30 minutes
+
+# 2 hours + 0.5 hours
+
+# 0.5 * 60 minutes = 30 minutes
+
+# 2.6 hours = 2 hours
+# 0.6 hours = 0.6 * 60 minutes = 36 minutes
+
+# float(input('Enter hours: '))
+
+# tach phần số thập phân
+hours = 2.75
+
+hour = int(hours)
+minute = (hours % 1) * 60
+
+print(hour, 'hours', int(minute), 'minutes')
+
