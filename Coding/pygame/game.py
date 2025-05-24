e47e52230efec2b02a234e2c4ceeee904d5bd993
hoangdesu <hoangdesu@gmail.com>
2025-05-24 15:35:19 +0700

    added playing character, randomize food position

--- Coding/pygame/game.py
+++ Coding/pygame/game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 pg.init()
 
@@ -12,12 +13,21 @@ screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 FPS = pg.time.Clock()
 
 # Load game sprites
-takoyaki_sprite = pg.image.load('takoyaki.png').convert_alpha()
-takoyaki_sprite = pg.transform.scale(takoyaki_sprite, (100, 100 * (350 / 339)))
+beo_sprite = pg.image.load('beo.png').convert_alpha()
+beo_sprite = pg.transform.scale(beo_sprite, (100, 100 * (350 / 339)))
 
-x = 0
+x = 400
+y = 300
 direction = 'right'
-speed = 5
+speed = 6
+
+
+takoyaki_sprite = pg.image.load('takoyaki.png').convert_alpha()
+takoyaki_sprite = pg.transform.scale(takoyaki_sprite, (80, 80 * (350 / 339)))
+
+tako_x = random.randint(10, 900)
+tako_y = random.randint(10, 700)
+
 
 # Game loop
 running = True
@@ -26,10 +36,19 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+            
+        # test random food position
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                tako_x = random.randint(10, 900)
+                tako_y = random.randint(10, 700)
     
     screen.fill((232, 241, 255))
     
-    screen.blit(takoyaki_sprite, (x, 300))
+    screen.blit(takoyaki_sprite, (tako_x, tako_y))
+    
+    screen.blit(beo_sprite, (x, y))
+
 
     # move the food left and right
     # if direction == 'right':
@@ -52,7 +71,14 @@ while running:
     elif key_pressed[pg.K_LEFT]:
         # if y <= SCREEN_HEIGHT - circle_radius:
             x -= speed
-                
+    
+    if key_pressed[pg.K_UP]:
+        # if y >= circle_radius:
+            y -= speed
+    elif key_pressed[pg.K_DOWN]:
+        # if y <= SCREEN_HEIGHT - circle_radius:
+            y += speed
+     
     
     pg.display.flip()
     FPS.tick(60)
